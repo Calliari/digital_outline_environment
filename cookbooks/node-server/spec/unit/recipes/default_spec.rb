7bf3b11e4b991c7311e1f1aac6e84be18b7a04ab
Calliari <ocaliari@spartaglobal.co>
2017-04-17 18:30:21 +0100

    default_spec.rb for testing setting up

--- cookbooks/node-server/spec/unit/recipes/default_spec.rb
+++ cookbooks/node-server/spec/unit/recipes/default_spec.rb
@@ -52,18 +52,17 @@ describe 'node-server::default' do
       expect(chef_run).to include_recipe('nodejs')
     end
 
-    # it 'should install pm2' do
-    #   expect(chef_run).to include_recipe 'nodejs::nodejs_from_package' 
-    # end
-
      it 'should install git' do
       expect(chef_run).to include_recipe('git::default')
     end
 
-    it 'should install om2 via :npm' do
-      expect(chef_run).to install_nodejs_npm('pm2')
-    end
+    # it 'should install pm2 via :npm' do
+    #   expect(chef_run).to install_nodejs_npm('pm2')
+    # end
 
+    # it 'should install pm2' do
+    #   expect(chef_run).to include_recipe 'nodejs::nodejs_from_package' 
+    # end
 
 
 
