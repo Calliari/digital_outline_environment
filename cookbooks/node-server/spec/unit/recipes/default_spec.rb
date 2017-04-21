acc40b1953b63cd6d5b6e9903546a52a44ec034b
Calliari <ocaliari@spartaglobal.co>
2017-04-21 12:02:07 +0100

    include the pm2 recipe

--- cookbooks/node-server/spec/unit/recipes/default_spec.rb
+++ cookbooks/node-server/spec/unit/recipes/default_spec.rb
@@ -55,21 +55,21 @@ describe 'node-server::default' do
       expect(template).to notify('service[nginx]').to(:reload)
    end
 
-    it 'should install nodejs::npm' do
-      expect(chef_run).to include_recipe('nodejs::npm')
-    end
+    # it 'should install nodejs::npm' do
+    #   expect(chef_run).to include_recipe('nodejs::npm')
+    # end
 
     it 'should a value for the magic_shell_environment' do
       expect(chef_run).to add_magic_shell_environment('MONGODB_URI')
     end
 
-    it 'should install pm2 via :npm' do
-      expect(chef_run).to install_nodejs_npm('pm2')
-    end
+    # it 'should install pm2 via :npm' do
+    #   expect(chef_run).to install_nodejs_npm('pm2')
+    # end
 
-    it 'should install pm2' do
-      expect(chef_run).to include_recipe 'nodejs::nodejs_from_package' 
-    end
+    # it 'should install pm2' do
+    #   expect(chef_run).to include_recipe 'nodejs::nodejs_from_package' 
+    # end
 
 
 
