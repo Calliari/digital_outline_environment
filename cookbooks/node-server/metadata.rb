e74edef57e6925ff269052e8f699ef20d207d891
Calliari <ocaliari@spartaglobal.co>
2017-04-23 16:46:17 +0100

    pm2 working without problem in the cookbooks

--- cookbooks/node-server/metadata.rb
+++ cookbooks/node-server/metadata.rb
@@ -25,7 +25,7 @@ chef_version '>= 12.9' if respond_to?(:chef_version)
  depends 'magic_shell'
 
 
-
+# https://github.com/Mindera/pm2-cookbook/blob/master/metadata.rb
 depends 'poise-javascript', '~> 1.1.0'
  
  # depends 'pm2', '~> 0.7.4'
