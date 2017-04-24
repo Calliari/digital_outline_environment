cb08ba3ffcc309a1ec8fdff741f37575342d74e7
Calliari <ocaliari@spartaglobal.co>
2017-04-24 10:00:46 +0100

    trying a include recipe - docker-pm2

--- cookbooks/node-server/metadata.rb
+++ cookbooks/node-server/metadata.rb
@@ -23,6 +23,7 @@ chef_version '>= 12.9' if respond_to?(:chef_version)
  depends 'git', '~> 6.0.0'  	# git dependencies
  depends 'mongodb3', '~> 5.3.0' # mongo dependencies https://supermarket.chef.io/cookbooks/mongodb3
  depends 'magic_shell'
+ depends 'docker', '~> 2.15.5'
 
 
 # https://github.com/Mindera/pm2-cookbook/blob/master/metadata.rb
