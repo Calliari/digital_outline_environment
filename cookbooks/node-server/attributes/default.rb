20f9da7cdf096f858dc10a6fb7427eca88050392
Calliari <ocaliari@spartaglobal.co>
2017-04-24 10:09:50 +0100

    trying a include recipe - docker-pm2

--- cookbooks/node-server/attributes/default.rb
+++ cookbooks/node-server/attributes/default.rb
@@ -2,4 +2,6 @@ default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = '6.10.0'
 default['nodejs']['binary']['checksum'] = '99c4136cf61761fac5ac57f80544140a3793b63e00a65d4a0e528c9db328bf40'
 
-default['docker-pm2']['bacon'] = 'true'
+# Used for the default recipe.
+default['poise-javascript']['install_nodejs'] = true
+default['poise-javascript']['install_iojs'] = false
--- cookbooks/node-server/recipes/default.rb
+++ cookbooks/node-server/recipes/default.rb
@@ -35,11 +35,14 @@ include_recipe "nodejs"
 include_recipe 'git'
 include_recipe 'apt'
 
-nodejs_npm 'pm2'
+# nodejs_npm 'pm2'
 
-include_recipe 'docker-pm2'
+
 
 # include_recipe 'pm2'
 
+# Default runtimes, last one will be the default.
+javascript_runtime 'iojs' if node['poise-javascript']['install_iojs']
+javascript_runtime 'nodejs' if node['poise-javascript']['install_nodejs']
 
-
+include_recipe 'pm2::default'
