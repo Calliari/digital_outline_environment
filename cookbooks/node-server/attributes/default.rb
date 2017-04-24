705933d6d2322d0d9fc4e9e01ed96ecd30c47abd
Calliari <ocaliari@spartaglobal.co>
2017-04-24 10:14:43 +0100

    trying a include recipe - docker-pm2

--- cookbooks/node-server/attributes/default.rb
+++ cookbooks/node-server/attributes/default.rb
@@ -5,3 +5,8 @@ default['nodejs']['binary']['checksum'] = '99c4136cf61761fac5ac57f80544140a3793b
 # Used for the default recipe.
 default['poise-javascript']['install_nodejs'] = true
 default['poise-javascript']['install_iojs'] = false
+
+
+default_unless['pm2']['pm2_version'] = 'latest'
+default_unless['pm2']['npm_version'] = 'latest'
+default_unless['pm2']['node_version'] = '4.5.0'
